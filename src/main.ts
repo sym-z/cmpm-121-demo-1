8b4b866920e8704186a139583e45ae6e63bbf794
sym-z <symz.exe@gmail.com>
2024-10-06 16:06:49 -0700

    Step 3 complete, auto-rescue initialized

--- src/main.ts
+++ src/main.ts
@@ -12,16 +12,24 @@ const main_text = "🐢";
 main_button.innerHTML = main_text;
 
 const main_amount_label = document.createElement("div");
-let main_amount : number = 0;
+let main_amount: number = 0;
 main_amount_label.innerHTML = `${main_amount} turtles rescued.`;
 
-main_button.onclick = () =>
-    {
-        main_amount++;
-        main_amount_label.innerHTML = `${main_amount} turtles rescued.`;
-    }
+main_button.onclick = () => {
+    rescueTurtle();
+};
 
+const auto_rescue_delay : number = 1000;
+setInterval(rescueTurtle, auto_rescue_delay)
 
 app.append(header);
 app.append(main_button);
 app.append(main_amount_label);
+
+
+function rescueTurtle()
+{
+  main_amount++;
+  main_amount_label.innerHTML = `${main_amount} turtles rescued.`;
+
+}
