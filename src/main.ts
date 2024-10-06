332516b75fba3b848b271852c02e55116e9b8a8c
sym-z <symz.exe@gmail.com>
2024-10-06 15:10:12 -0700

    Step 1 complete, turtle button activated

--- src/main.ts
+++ src/main.ts
@@ -2,9 +2,14 @@ import "./style.css";
 
 const app: HTMLDivElement = document.querySelector("#app")!;
 
-const gameName = "Testing correct Git Account...";
-document.title = gameName;
-
+const gameName = "Jack's Temporary Clicky Game Title";
 const header = document.createElement("h1");
 header.innerHTML = gameName;
+document.title = gameName;
+
+const main_button = document.createElement("button")
+const main_text = "🐢"
+main_button.innerHTML = main_text;
+
 app.append(header);
+app.append(main_button)
