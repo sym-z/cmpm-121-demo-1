4a59e8ee56741cfe1c38cb19f52f7c288d81ae99
sym-z <symz.exe@gmail.com>
2024-10-06 17:22:35 -0700

    Step 4 complete, normalized increase of auto-turtle across frame rates

--- src/main.ts
+++ src/main.ts
@@ -15,21 +15,48 @@ const main_amount_label = document.createElement("div");
 let main_amount: number = 0;
 main_amount_label.innerHTML = `${main_amount} turtles rescued.`;
 
+// Changing this later to allow for upgrades.
+const rescue_per_click : number = 1;
 main_button.onclick = () => {
-    rescueTurtle();
+  rescueTurtle(rescue_per_click);
 };
 
-const auto_rescue_delay : number = 1000;
-setInterval(rescueTurtle, auto_rescue_delay)
+// Start the auto-click
+requestAnimationFrame(autoTurtle)
 
-app.append(header);
-app.append(main_button);
-app.append(main_amount_label);
+// Append elements
+appendToApp();
 
-
-function rescueTurtle()
-{
-  main_amount++;
+function rescueTurtle(num_turts : number) {
+  main_amount += num_turts;
   main_amount_label.innerHTML = `${main_amount} turtles rescued.`;
+}
 
+// Time since last auto-turtle
+let last_auto_tick : number = 0;
+// Changing this once upgrades are implemented.
+const rescue_per_auto : number = 1;
+function autoTurtle(timestamp : number)
+{
+    // Calculates the time in seconds, based off of the time the
+    // last frame finished
+    const time_sec = timestamp / 1000;
+    
+    // Floor the value, to identify whether or not the code has
+    // auto-turtle'd within the last second since the start of the
+    // game.
+    if (Math.floor(time_sec) > last_auto_tick)
+    {
+        // Store the most recent second that the code auto-turtle'd
+        last_auto_tick = Math.floor(time_sec)
+        
+        rescueTurtle(rescue_per_auto)
+    }
+    requestAnimationFrame(autoTurtle)
+}
+function appendToApp()
+{
+    app.append(header);
+    app.append(main_button);
+    app.append(main_amount_label);
 }
