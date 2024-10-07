04da9f03a0ae2c20be8ada6cb1651ab37ce46ee1
sym-z <symz.exe@gmail.com>
2024-10-06 17:45:50 -0700

    Step 5 Complete, Turtle Rescue Squads can now be trained to automatically rescue turtles, at the price of 10 turtles.

--- src/main.ts
+++ src/main.ts
@@ -7,56 +7,84 @@ const header = document.createElement("h1");
 header.innerHTML = gameName;
 document.title = gameName;
 
+// MAIN BUTTON SETUP
 const main_button = document.createElement("button");
 const main_text = "🐢";
 main_button.innerHTML = main_text;
-
 const main_amount_label = document.createElement("div");
-let main_amount: number = 0;
-main_amount_label.innerHTML = `${main_amount} turtles rescued.`;
-
+let total_turtles: number = 0;
+main_amount_label.innerHTML = `${total_turtles} turtles rescued.`;
 // Changing this later to allow for upgrades.
-const rescue_per_click : number = 1;
+const rescue_per_click: number = 1;
 main_button.onclick = () => {
   rescueTurtle(rescue_per_click);
 };
 
+// UPGRADE AUTO BUTTON SETUP
+const upgrade_auto_button = document.createElement("button")
+const upgrade_auto_text = "Click me to train turtle rescue squads! (Cost: 10 Turtles)"
+upgrade_auto_button.innerHTML = upgrade_auto_text
+// Button is disabled at start.
+upgrade_auto_button.disabled = true;
+
+
+// Will change if I plan to scale
+const upgrade_auto_cost : number = 10
+upgrade_auto_button.onclick = () => 
+    {
+        if(total_turtles >= upgrade_auto_cost)
+            {
+                // Rescuing negative turtles "spends" them on an
+                // upgrade.
+                rescueTurtle(-upgrade_auto_cost)
+                // Upgrade the amount of turtles rescued every second
+                rescue_per_auto += 1;
+            }
+        
+    }
+
 // Start the auto-click
-requestAnimationFrame(autoTurtle)
+requestAnimationFrame(autoTurtle);
 
 // Append elements
 appendToApp();
 
-function rescueTurtle(num_turts : number) {
-  main_amount += num_turts;
-  main_amount_label.innerHTML = `${main_amount} turtles rescued.`;
+function rescueTurtle(turts_to_rescue: number) {
+  total_turtles += turts_to_rescue;
+  if(total_turtles < upgrade_auto_cost)
+    {
+        upgrade_auto_button.disabled = true;
+    }
+  else if (upgrade_auto_button.disabled)
+    {
+        upgrade_auto_button.disabled = false;
+    }
+  main_amount_label.innerHTML = `${total_turtles} turtles rescued.`;
 }
 
 // Time since last auto-turtle
-let last_auto_tick : number = 0;
-// Changing this once upgrades are implemented.
-const rescue_per_auto : number = 1;
-function autoTurtle(timestamp : number)
-{
-    // Calculates the time in seconds, based off of the time the
-    // last frame finished
-    const time_sec = timestamp / 1000;
-    
-    // Floor the value, to identify whether or not the code has
-    // auto-turtle'd within the last second since the start of the
-    // game.
-    if (Math.floor(time_sec) > last_auto_tick)
-    {
-        // Store the most recent second that the code auto-turtle'd
-        last_auto_tick = Math.floor(time_sec)
-        
-        rescueTurtle(rescue_per_auto)
-    }
-    requestAnimationFrame(autoTurtle)
+let last_auto_tick: number = 0;
+// No auto-click at the start.
+let rescue_per_auto: number = 0;
+function autoTurtle(timestamp: number) {
+  // Calculates the time in seconds, based off of the time the
+  // last frame finished
+  const time_sec = timestamp / 1000;
+
+  // Floor the value, to identify whether or not the code has
+  // auto-turtle'd within the last second since the start of the
+  // game.
+  if (Math.floor(time_sec) > last_auto_tick) {
+    // Store the most recent second that the code auto-turtle'd
+    last_auto_tick = Math.floor(time_sec);
+
+    rescueTurtle(rescue_per_auto);
+  }
+  requestAnimationFrame(autoTurtle);
 }
-function appendToApp()
-{
-    app.append(header);
-    app.append(main_button);
-    app.append(main_amount_label);
+function appendToApp() {
+  app.append(header);
+  app.append(main_button);
+  app.append(upgrade_auto_button);
+  app.append(main_amount_label);
 }
